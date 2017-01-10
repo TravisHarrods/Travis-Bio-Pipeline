c14ae8556a591f94185243d8e2f2c411b1c45319
Travis Harrods <travis.harrods@gmail.com>
2017-01-10 17:01:31 +0100

    Add the pipeline launching script: bioPipeline.pl.

---
+++ bin/bioPipeline.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+
+use 5.014;
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+use Travis::Utilities::Log;
+use Travis::Bio::Pipeline;
+
+my $log = Travis::Utilities::Log->new();
+
+my $input  = ''; # An input path (single file, list of files, directory, ...)
+my $output = ''; # An output path to a directory
+my $format = ''; # A standard file format
+
+my $pipeline = '';
+
+my $list_plugins; # Boolean indicating if plugin list had to be shown
+
+eval {
+   GetOptions(
+      '-input|i=s'      => \$input,
+      '-output|o=s'     => \$output,
+      '-format|f=s'     => \$format,
+      '-pipeline|p=s'   => \$pipeline,
+      '-list-plugins|l' => \$list_plugins
+   );
+};
+if( $@ ) {
+   $log->fatal('Argument value error: '.$@);
+}
+# Only show the list of plugins
+if( defined($list_plugins) ) {
+   my $bs = Travis::Bio::Pipeline->new();
+   $bs->listPlugins();
+   exit(1);
+}
+
+# No help or plugin list asked
+if($input eq '') {
+   $log->fatal('You must provide an input path/file.');
+}
+
+$log->info('Reading input sequences...');
+my $bs = Travis::Bio::Pipeline->new( 
+   input            => $input,
+   format           => $format,
+   pipeline         => $pipeline
+);
+
+
+# If required, change the output directory
+if( $output ne '' ) {
+   $log->trace('Changing output directory...');
+   $bs->changeOutputDirectories($output);
+}
+
+# Launch the pipeline
+$log->info('Executing the pipeline...');
+$bs->executePipeline();
+
+
+#say $factory->countSequences();
+#say $factory->sequences()->[0]->getOutputPath();
+#$factory->writeSequence('YALI0A');
+
+#my $seq = TRAVIS::BioSeq::sequence->new( input => $input );
+#say $seq->seqId();
+#$seq->setOutputPath('test.embl');
+#$seq->sortFeatures();
+#$seq->write();
